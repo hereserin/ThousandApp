b2b106d41b29d14db6e4480d45db3c6ec22d8359
Erin Marshall <erin.e.mars@gmail.com>
2019-04-08 21:04:13 -0400

    Set api fetches back to normal

--- util/pages_api_util.js
+++ util/pages_api_util.js
@@ -1,33 +1,33 @@
-export const fetchPages = () => {
-  return $.ajax({
-    method: "GET",
-    url: `http://localhost:3000/api/pages`
-  });
-};
-
 // export const fetchPages = () => {
 //   return $.ajax({
 //     method: "GET",
-//     url: `https://thousand-app.herokuapp.com/api/pages`
+//     url: `http://localhost:3000/api/pages`
 //   });
 // };
-// header: { "Content-Type": "application/json" }
 
-export const searchPages = search => {
+export const fetchPages = () => {
   return $.ajax({
     method: "GET",
-    url: `http://localhost:3000/api/search`,
-    data: search
+    url: `https://thousand-app.herokuapp.com/api/pages`
   });
 };
+// header: { "Content-Type": "application/json" }
 
 // export const searchPages = search => {
 //   return $.ajax({
 //     method: "GET",
-//     url: `https://thousand-app.herokuapp.com/api/search`,
+//     url: `http://localhost:3000/api/search`,
 //     data: search
 //   });
 // };
+
+export const searchPages = search => {
+  return $.ajax({
+    method: "GET",
+    url: `https://thousand-app.herokuapp.com/api/search`,
+    data: search
+  });
+};
 
 // export const fetchPages = () => {
 //   return $.ajax({
