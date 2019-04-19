47bb4de2ab3b42877d84452d72d728100ecff9c3
Erin Marshall <erin.e.mars@gmail.com>
2019-04-19 11:28:45 -0400

    Move pagerank label

--- components/page_index_item.jsx
+++ components/page_index_item.jsx
@@ -4,9 +4,9 @@ const PageIndexItem = props => {
   return (
     <div className="page-index-item">
       <a href={props.url}>
-        <p className="page-clickable-title">pageRank: {props.title}</p>
+        <p className="page-clickable-title">{props.title}</p>
       </a>
-      <p className="page-rank">{props.pageRank}</p>
+      <p className="page-rank">pageRank: {props.pageRank}</p>
       <p className="page-url">{props.url}</p>
       <p />
       <p className="page-excerpt">{props.excerpt}</p>
